035e35df3375b5c5f6c8378449614bf82a55b5e7
CodeFusion2024 <codefusion.tech.in@gmail.com>
2025-04-14 00:55:01 +0530

    nxhfcgh

--- src/model/Category.js
+++ src/model/Category.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const categorySchema = new mongoose.Schema({
+  catId: { type: String, unique: true },
   name: { type: String, required: true },
   company: { type: String },
   types: [String],
@@ -8,8 +9,16 @@ const categorySchema = new mongoose.Schema({
   color: [String],
   rating: { type: Number, default: 0 },
   review: [String],
-  storeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Store' }  // To link category to a store
+  storeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Store' }
 }, { timestamps: true });
+
+categorySchema.pre('save', async function (next) {
+  if (!this.catId) {
+    const count = await mongoose.model('Category').countDocuments();
+    this.catId = `CAT${(count + 1).toString().padStart(3, '0')}`;
+  }
+  next();
+});
 
 const Category = mongoose.model('Category', categorySchema);
 export default Category;
